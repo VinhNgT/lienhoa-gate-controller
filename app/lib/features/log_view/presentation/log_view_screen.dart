2c94f141b15c2b1afffacf2841d719d864deb0ef
Victor Nguyen <victorpublic0000@gmail.com>
2024-10-05 00:00:36 +0700

    feat: add preliminary log view screen

--- app/lib/features/log_view/presentation/log_view_screen.dart
+++ app/lib/features/log_view/presentation/log_view_screen.dart
@@ -1,6 +1,10 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:lienhoa_gate_controller/constants/app_sizes.dart';
+import 'package:lienhoa_gate_controller/utils/context_extensions.dart';
+import 'package:material_symbols_icons/material_symbols_icons.dart';
 
 @RoutePage()
 class LogViewScreen extends HookConsumerWidget {
@@ -10,10 +14,35 @@ class LogViewScreen extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Log View'),
+        title: const Text('Nhật ký'),
+        actions: [
+          FilledButton.tonalIcon(
+            onPressed: () {},
+            icon: const Icon(Symbols.center_focus_strong),
+            label: const Text('Xử lý ảnh hiện tại'),
+          ),
+          const Gap(kSize_8),
+          FilledButton.icon(
+            onPressed: () {},
+            icon: const Icon(Symbols.settings),
+            label: const Text('Cài đặt'),
+          ),
+          const Gap(kSize_8),
+        ],
       ),
-      body: const Center(
-        child: Text('Log View'),
+      body: Container(
+        margin: const EdgeInsets.only(
+          left: kSize_8,
+          right: kSize_8,
+          bottom: kSize_8,
+        ),
+        decoration: BoxDecoration(
+          border: Border.all(
+            color: context.colorScheme.outline,
+            width: 2,
+          ),
+          borderRadius: BorderRadius.circular(kSize_8),
+        ),
       ),
     );
   }
