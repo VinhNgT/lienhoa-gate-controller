c9c0d6de29ee88ddb53331aae11f3b13917070d5
Victor Nguyen <victorpublic0000@gmail.com>
2024-10-09 00:02:31 +0700

    feat: add way to pause RaspiDistanceSensorValueStream

--- app/lib/features/raspi_gate/application/raspi_gate_control_providers.dart
+++ app/lib/features/raspi_gate/application/raspi_gate_control_providers.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:lienhoa_gate_controller/features/raspi_gate/data/raspi_gate_control_client.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
@@ -5,12 +7,34 @@ part 'raspi_gate_control_providers.g.dart';
 
 /// Return the distance sensor value from the Raspberry Pi in cm.
 @riverpod
-Stream<int> raspiDistanceSensorValueStream(
-  RaspiDistanceSensorValueStreamRef ref,
-) {
-  final client = ref.watch(raspiGateControlClientProvider);
+class RaspiDistanceSensorValueStream extends _$RaspiDistanceSensorValueStream {
+  late StreamController<int> _controller;
+  late StreamSubscription<int> __valueStreamSub;
 
-  return Stream.periodic(const Duration(seconds: 1), (_) {
-    return client.getDistanceSensorValue();
-  }).asyncMap((event) => event);
+  @override
+  Stream<int> build() {
+    final client = ref.watch(raspiGateControlClientProvider);
+    _controller = StreamController<int>();
+
+    __valueStreamSub = Stream.periodic(const Duration(seconds: 1), (_) {
+      return client.getDistanceSensorValue();
+    }).asyncMap((event) => event).listen((event) {
+      _controller.add(event);
+    });
+
+    ref.onDispose(() {
+      __valueStreamSub.cancel();
+      _controller.close();
+    });
+
+    return _controller.stream;
+  }
+
+  void pause() {
+    __valueStreamSub.pause();
+  }
+
+  void resume() {
+    __valueStreamSub.resume();
+  }
 }
